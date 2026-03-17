9800870a23991f5977424e01584f0fbe90a37dc6
MozzyMchanz <mozzymchanz@gmail.com>
2026-03-17 09:04:48 +0300

    commit

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,61 @@
-using System;
+ using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("\nJournal Menu:");
+            Console.WriteLine("1. Write New Entry");
+            Console.WriteLine("2. Display Journal");
+            Console.WriteLine("3. Save Journal");
+            Console.WriteLine("4. Load Journal");
+            Console.WriteLine("5. Exit");
+
+            Console.Write("Choose an option: ");
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine($"\nPrompt: {prompt}");
+
+                Console.Write("Your response: ");
+                string response = Console.ReadLine();
+
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._promptText = prompt;
+                entry._entryText = response;
+
+                journal.AddEntry(entry);
+            }
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.Write("Enter filename to save: ");
+                string filename = Console.ReadLine();
+
+                journal.SaveToFile(filename);
+                Console.WriteLine("Journal saved successfully!");
+            }
+            else if (choice == 4)
+            {
+                Console.Write("Enter filename to load: ");
+                string filename = Console.ReadLine();
+
+                journal.LoadFromFile(filename);
+                Console.WriteLine("Journal loaded successfully!");
+            }
+        }
     }
 }
---
+++ week02/Journal/Promptgenerator.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>()
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "What made me smile today?",
+        "What challenge did I overcome today?",
+        "What is one thing I learned today?",
+        "If I could relive today, what would I change?"
+    };
+
+    public string GetRandomPrompt()
+    {
+        Random rand = new Random();
+        int index = rand.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
